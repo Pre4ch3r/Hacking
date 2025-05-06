b10d33ac32e9f6fb3ca8fa6ee3b904ac31ed5154
Pre4ch3r <preacher.fulltime@protonmail.com>
2025-05-06 19:52:00 -0300

    Mudanças no header da pagina

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Quartz 4",
+    pageTitle: "Pr3ach3r's Cybersecurity Stuff",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
